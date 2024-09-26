47a67f1eef35249f6365e49ebddd5a1dd191046d
trannnvuuphat <tranvuphato@gmail.com>
2024-09-26 15:09:46 +0700

    firebase install

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDq1immgd9S3KMSgHfa5f5DiSAmWQ7VHAw",
   authDomain: "my-socket-app.firebaseapp.com",
@@ -18,17 +13,10 @@ const firebaseConfig = {
   measurementId: "G-WF9L2P0FQL"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-//const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export {auth, db} ;
 
-// Khởi tạo Firebase
-//const app = initializeApp(firebaseConfig);
-
-// Khởi tạo dịch vụ
-//export const auth = getAuth(app);
-//export const db = getFirestore(app);
